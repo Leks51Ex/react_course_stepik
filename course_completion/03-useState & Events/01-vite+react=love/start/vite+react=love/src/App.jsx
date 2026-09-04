9aeddb448ee3cd5409b414c0b2fdc7a1b1240255
Leks51Ex <bulat_lesha@mail.ru>
2026-09-04 13:14:18 +0300

    additional rendering state

--- course_completion/03-useState & Events/01-vite+react=love/start/vite+react=love/src/App.jsx
+++ course_completion/03-useState & Events/01-vite+react=love/start/vite+react=love/src/App.jsx
@@ -3,24 +3,62 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const [isOpen, setIsOpen] = useState(false);
+
   // function handleClick() {
   //   alert("hello");
   // }
 
+  function toggleIsOpen() {
+    setIsOpen((prev) => !prev);
+  }
+
   return (
     <>
-      <h1>Vite + React = Love</h1>
+      {isOpen ? (
+        <span onClick={toggleIsOpen} className="cross">
+          &times;
+        </span>
+      ) : (
+        <button onClick={toggleIsOpen}>Начать</button>
+      )}
 
-      <hr />
+      {isOpen && (
+        <>
+          <h1>Vite + React = {count >= 3 ? "Love" : ""}</h1>
 
-      <div className="card">
-        <p className="count-paragraph">count is {count}</p>
-        <div className="increment-buttons">
-          <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
-          <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
-          <button onClick={() => setCount(0)}>Reset</button>
-        </div>
-      </div>
+          <div className="logo-container">
+            <img
+              className={`logo ${count >= 1 ? "active" : ""}`}
+              src="/vite.svg"
+              alt=""
+            />
+            <p>+</p>
+            <img
+              className={`logo ${count >= 2 ? "active" : ""}`}
+              src="/react.svg"
+              alt=""
+            />
+            <p>=</p>
+            <img
+              className={`logo ${count >= 3 ? "active" : ""}`}
+              src="/love.svg"
+              alt=""
+            />
+          </div>
+
+          <hr />
+
+          <div className="card">
+            <p className="count-paragraph">count is {count}</p>
+            <div className="increment-buttons">
+              <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+              <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
+              <button onClick={() => setCount(0)}>Reset</button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
